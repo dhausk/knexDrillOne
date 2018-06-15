aaff4ceec3f2260ec3c08f70229a9c3cd9d10ab4
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-06-15 10:27:27 -0600

    fixed table name

--- migrations/20180613125752_resolution.js
+++ migrations/20180613125752_resolution.js
@@ -1,11 +1,11 @@
 
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('resolution', function (table) {
+  return knex.schema.createTable('resolutions', function (table) {
     table.increments('id');
     table.date('dueDate');
     table.text('resolution');
   });
 };
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('resolution')
+  return knex.schema.dropTableIfExists('resolutions')
 };
